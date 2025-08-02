3a37037e444f19af31118adb86480a5e4e0d2802
Priyadharshini M <priyadharshini02843@gmail.com>
2025-08-02 11:18:43 +0530

    Update Calculator.java

--- src/main/java/Week1/Calculator.java
+++ src/main/java/Week1/Calculator.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
 
+class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1, num2;
+        char op;
+        double result = 0;
+        System.out.print("Enter first number: ");
+        num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        num2 = sc.nextDouble();
+        System.out.print("Enter the operator (+, -, *, /): ");
+        op = sc.next().charAt(0);
+        if (op == '+')
+            result = num1 + num2;
+        else if (op == '-')
+            result = num1 - num2;
+        else if (op == '*')
+            result = num1 * num2;
+        else if (op == '/')
+            result = num1 / num2;
+        else
+            System.out.println("Invalid Operator");
+            System.out.println("Result: " + result);
+        sc.close();
+    }
+}
