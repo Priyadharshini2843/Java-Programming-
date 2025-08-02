edf9ef3f01932ae2ebb318ad9a4f1e42d3f93731
Priyadharshini M <priyadharshini02843@gmail.com>
2025-08-02 11:13:48 +0530

    Update Team.java

--- src/main/java/Week1/Team.java
+++ src/main/java/Week1/Team.java
@@ -1,1 +1,16 @@
-
+import java.util.Scanner;
+class Team{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the points: ");
+int points=sc.nextInt();
+System.out.print("Enter the fouls: ");
+short fouls=sc.nextShort();
+boolean winStatus=(points>=50) && (fouls<=5);
+System.out.println("Team Game Status: ");
+System.out.println("Points: "+points);
+System.out.println("Fouls: "+fouls);
+System.out.println("Win Status: "+winStatus);
+}
+}
