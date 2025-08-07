1c987259c251fa3bef86a04516a3dc5bce18f117
Priyadharshini M <priyadharshini02843@gmail.com>
2025-08-08 00:02:55 +0530

    Created

---
+++ src/main/java/Week4/thisKey.java
@@ -0,0 +1,15 @@
+public class thisKey {
+    float amount;
+    thisKey(float amount) {
+        this.amount = amount;
+    }
+    void display() {
+        System.out.println("Amount: "+amount);
+    }
+    public static void main(String[] args) {
+        thisKey obj = new thisKey(8000f);
+        System.out.println(" PRIYADHARSHINI . M (2024503501) || Week4 Program 3 ");
+        obj.display();
+    }
+
+}
